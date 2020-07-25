3cdf73f30208843ef09bf2ab38e7bfd6afa96a0d
Fabio Silva Ferreira <fabiosvf@gmail.com>
2020-07-25 01:59:27 -0300

    Nível 01 - TypeScript - Tipando objetos e vetores - Arrays com tipagem mista

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,13 @@ import createUser from './services/CreateUser';
 export function helloWorld(request: Request, response: Response) {
   const user = createUser({
     email: 'fabiosvf@gmail.com', 
-    password: '123456'
+    password: '123456',
+    techs: [
+      'Node.js', 
+      'ReatJS', 
+      'React Native',
+      { title: 'Javascript', experience: 100 }
+    ],
   });
   
   return response.json({message: "Hello World"});
